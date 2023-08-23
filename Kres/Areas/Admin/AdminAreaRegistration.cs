b1d2f32ab0823c85a4019ec79b9607c297594455
Mert Guner <mertcannguner@gmail.com>
2023-08-23 22:02:03 +0300

    First Commit
    First Commit

---
+++ Kres/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kres.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                // return View(model: "Admin area home controller");
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                   defaults: new
+                   {
+                       controller = "Homes",
+                       action = "Index",
+                       id = UrlParameter.Optional
+                   }
+            //  new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Kres/Areas/Admin/Controllers/HomesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kres.Areas.Admin.Controllers
+{
+    public class HomesController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
